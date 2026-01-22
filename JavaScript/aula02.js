49923b9cdd748cda24eab5632cd5194a6e94365a
ICARO FERREIRA FRANKLIN <icarofranklins@gmail.com>
2026-01-22 09:48:08 -0300

    Create aula02.js

---
+++ JavaScript/aula02.js
@@ -0,0 +1,4 @@
+"use strict"; // Habilita o modo estrito para evitar erros comuns
+let nome = "Ícaro"; // Declara uma variável chamada 'nome' e atribui o valor "Ícaro"
+console.log("Nome: " + nome); // Exibe o nome na console
+// Se não usar o modo estrito, erros como variáveis não declaradas podem passar despercebidos, como o let nome acima.
